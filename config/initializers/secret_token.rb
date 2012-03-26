13adbef54112437e5c079da550ce84cf302d7e9c
hoganord <alexwood@manchester.ac.uk>
2012-03-26 14:56:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnmiauthMongodb::Application.config.secret_token = 'b06aba413a963695568f232e0948cd417a147cd87a251d478d0a24d0c5df711731e5650d1d8cdc1e87b60fa50eb5adb0cabc9c0205e77f387d7193ff678fb275'
