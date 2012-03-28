858dfc3147929759c9526f1b09bb0d9409adbbbc
hoganord <alexwood@manchester.ac.uk>
2012-03-28 15:32:45 +0100

    Added email on login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User
   field :uid, :type => String
   field :name, :type => String
   field :email, :type => String
-  attr_protected :provider, :uid, :name, :email
+  attr_protected :provider, :uid, :name
+  
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+         user.name = auth['info']['name'] || ""
+         user.email = auth['info']['email'] || nil
+      end
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 OnmiauthMongodb::Application.routes.draw do
+
+  resources :users, :only => :show
+
+  get "home/index"
+
+  match '/auth/:provider/callback' => 'sessions#create'
+  
+  match '/auth/failure' => 'sessions#failure'
+  
+  match '/signout' => 'sessions#destroy', :as => :signout
+  
+  match '/signin' => 'sessions#new', :as => :signin
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +61,8 @@ OnmiauthMongodb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index' 
+  resources :users, :only => [ :show, :edit, :update ]
 
   # See how all your routes lay out with "rake routes"
 
